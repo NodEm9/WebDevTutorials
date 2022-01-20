f482b9cbffe0070a288455f62d416a9a7ee5d1b1
NodEm9 <42060362+NodEm9@users.noreply.github.com>
2022-01-20 23:40:44 +0100

    Use foreach method to add the open class for hmaburger menu

--- script.js
+++ script.js
@@ -6,11 +6,10 @@ let showOpen = false;
 const toggleBurger = () => {
   if (!showOpen) {
     mainNav.forEach((item) => item.classList.add("open"));
-
     showOpen = true;
+    
   } else {
     mainNav.forEach((item) => item.classList.remove("open"));
-
     showOpen = false;
   }
 };
